0ce37c602de0f4497de95e8b5553c55247c99229
Sylvain75 <sylvoz75@gmail.com>
2016-02-08 09:24:00 +1100

    quantity line_items working

--- db/migrate/20160204115914_create_line_items.rb
+++ db/migrate/20160204115914_create_line_items.rb
@@ -1,7 +1,7 @@
 class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
-      t.integer :quantity
+      t.integer :quantity, :default => 1 
       t.integer :product_id
       t.integer :cart_id
       t.integer :order_id
@@ -9,4 +9,11 @@ class CreateLineItems < ActiveRecord::Migration
       t.timestamps null: false
     end
   end
+  def self.up
+      add_column :line_items, :quantity, :integer, :default => 1
+  end
+  def self.down
+     remove_column :line_items, :quantity
+  end 
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204120039) do
+ActiveRecord::Schema.define(version: 20160207113629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,12 @@ ActiveRecord::Schema.define(version: 20160204120039) do
   end
 
   create_table "line_items", force: :cascade do |t|
-    t.integer  "quantity"
+    t.integer  "quantity",   default: 1
     t.integer  "product_id"
     t.integer  "cart_id"
     t.integer  "order_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "orders", force: :cascade do |t|
